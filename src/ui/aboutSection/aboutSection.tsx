58e215d1cb101c7a8f53edbf6a8d6f14face4d1a
김동섭 <dongsubk@umich.edu>
2025-06-20 21:38:02 -0400

    about section header + resume download button implementation

--- src/ui/aboutSection/aboutSection.tsx
+++ src/ui/aboutSection/aboutSection.tsx
@@ -1,19 +1,31 @@
+"use client";
+
 import styles from "./aboutSection.module.css";
 import EmergingElement from "@ui/emergingElements/emergingElements";
-
+import Icon from "@ui/icon/icon";
 export default function AboutSection() {
+  const handleDownload = () => {
+    // Create a link element to trigger the download
+    const link = document.createElement("a");
+    link.href = "/resume.pdf"; // This will look for resume.pdf in the public directory
+    link.download = "Dongsub_Kim_Resume.pdf"; // This sets the filename for download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <section className={styles.container} id="about">
       <EmergingElement>
-        <h1 className={styles.title}>About Me</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-          quos. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit.
-        </p>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.title}>About Me</h1>
+          <button className={styles.button} onClick={handleDownload}>
+            <span className={styles.buttonText}>Resume</span>
+            <span className={styles.buttonIcon}>
+              <Icon name="download" size={35} />
+            </span>
+          </button>
+        </div>
       </EmergingElement>
     </section>
   );
