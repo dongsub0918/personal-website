9b6816553134d2dc53c9a0e6d3612b92dca3751b
김동섭 <dongsubk@umich.edu>
2025-06-25 13:45:40 -0400

    bio description content in progress

--- src/lib/content/bioCard.ts
+++ src/lib/content/bioCard.ts
@@ -3,19 +3,28 @@ const bioCardsData = [
     icon: "home",
     title: "Originated From",
     description: `Seongnam, Korea 🇰🇷
-I was born and raised in Korea, educated in Korea, and completed my mandatory military service in Korea.
-My Korean heritage is a core part of who I am—I carry it with pride.
-I love my family and friends in Korea. While I strongly identify as Korean, I also see myself as a global citizen.`,
+    I was born and raised in Korea, educated in Korea, and completed my mandatory military service in Korea.
+    My Korean heritage is a core part of who I am—I carry it with pride.
+    I love my family and friends in Korea. While I strongly identify as Korean, I also see myself as a global citizen.`,
   },
   {
     icon: "location",
     title: "Currently Based In",
     description: `Ann Arbor, MI, USA 🇺🇸
-I moved to the United States for my master's degree in Computer Science at the University of Michigan.
-I'm currently in the United States, and I'm enjoying my life in the United States.
-I'm a global citizen, and I'm proud of my identity.`,
+    I moved to the United States for my bachelor's degree in Computer Science at the University of Michigan.
+    After graduating and a year of experience in the industry, I am currently a research assistant at the University of Michigan.
+    Ann Arbor has been my home for the past 4 years, and always has been the best place to live, study, and work in peace.`,
   },
-  { icon: "code", title: "Coding", description: "Coding" },
+  {
+    icon: "code",
+    title: "Coding",
+    description: `I'm a full-stack developer.
+    I value understanding technology from the ground up. I don’t use tools blindly—I strive to grasp the underlying mechanisms before adopting them.
+    At the same time, I believe the best products are simple, intuitive, and well-encapsulated.
+    To me, simplicity is the ultimate sophistication.
+
+`,
+  },
   { icon: "heart", title: "Hobby", description: "Hobby" },
 ];
 
